3b1a16b8cc2e58e3a5527f892e95284be20d2645
kanziw <kanziwoong@gmail.com>
2018-02-19 17:46:28 +0900

    작성중인 포스트 임시저장

--- _codes/run.js
+++ _codes/run.js
@@ -6,5 +6,7 @@
 const di = {};
 
 import functionalJavascript01 from './2017-01-09-functional-javascript-01';
+import functionalJavascript02 from './2017-01-17-functional-javascript-02';
 
 functionalJavascript01(di);
+functionalJavascript02(di);
